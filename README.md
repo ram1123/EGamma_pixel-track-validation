c4006b039ca4caa112c46da5c2c86d1b77094b61
Ram Krishna Sharma <ram.krishna.sharma@cern.ch>
2021-10-07 16:02:11 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # pixel-track-validation
+
+- Step - 1: Get configuration file
+- Step - 2: Run the configuration file using `cmsRun`
+    - Step - 2(a): Submit the crab jobs with large statistics
+- Step - 3: Analyze the output
+
+# Crab job submission
+
+```bash
+cmsenv
+voms-proxy-init --voms cms --valid 168:00
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+crab-dev submit crab_config_default.py
+crab-dev submit crab_config_petatrk.py
+```
+
+```bash
+crab status -d crab_ZprimeToEE_M6000_TuneCP5_14TeV_default/crab_crab_ZprimeToEE_M6000_TuneCP5_14TeV_default
+
+```
---
+++ crab_config_petatrk.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+# config.section_('General')
+config.General.requestName = 'crab_ZprimeToEE_M6000_TuneCP5_14TeV_PetaTrk'
+config.General.workArea = 'crab_ZprimeToEE_M6000_TuneCP5_14TeV_PetaTrk'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+# config.section_('JobType')
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'hlt_12_0_1_patatrack.py'
+config.JobType.numCores = 4
+
+# config.Data.inputDBS = 'phys03'
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.maxMemoryMB = 1000
+
+# config.JobType.numCores = 8
+config.Data.inputDataset ='/ZprimeToEE_M-6000_TuneCP5_14TeV-pythia8/Run3Winter21DRMiniAOD-FlatPU0to80FEVT_112X_mcRun3_2021_realistic_v16-v2/GEN-SIM-DIGI-RAW'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+
+config.Data.outLFNDirBase = '/store/group/phys_egamma/Run3TriggerStudies/PixelTrackValidation/PetaTrk'
+config.Data.publication = False
+config.Site.storageSite = 'T2_CH_CERN'
